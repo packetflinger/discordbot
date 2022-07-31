c3f247d243783d33d2ffa2a5e5ac297922c31733
Joe Reid <joe@joereid.com>
2022-07-31 03:01:11 +0000

    added server status for !q2 command

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"net"
 	"net/http"
 	"io"
 	"os"
 	"os/signal"
+	"strconv"
 	"strings"
 	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -45,7 +49,6 @@ func main() {
 	}
 
 	// Wait here until CTRL-C or other term signal is received.
-	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
@@ -66,6 +69,20 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// testing
 	if m.Content == "ping" {
 		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+
+	if strings.HasPrefix(m.Content, "!q2 ") {
+		parts := strings.Split(m.Content, " ")
+		if len(parts) < 2 {
+			return
+		}
+
+		q2server := parts[1]
+		go func(srv string, ch string) {
+			status := ServerStatus(srv)
+			s.ChannelMessageSend(ch, status)
+		}(q2server, m.ChannelID)
+		return
 	}
 
 	// only process attachments in designated channels
@@ -145,4 +162,75 @@ func Contains(needle string, haystack []string) bool {
 	}
 
 	return false
+}
+
+//
+// Fetch server status
+//
+func ServerStatus(q2server string) string {
+	p := make([]byte, 1500)
+
+	// only use IPv4
+	conn, err := net.Dial("udp4", q2server)
+	if err != nil {
+		fmt.Printf("Connection error %v\n", err)
+		return q2server + " - Connection error"
+	}
+	defer conn.Close()
+	conn.SetReadDeadline(time.Now().Add(1 * time.Second))
+
+	cmd := []byte{0xff, 0xff, 0xff, 0xff}
+	cmd = append(cmd, "status"...)
+	fmt.Fprintln(conn, string(cmd))
+
+	_, err = bufio.NewReader(conn).Read(p)
+	if err != nil {
+		fmt.Println("Read error:", err)
+		return "Read error"
+	}
+
+	lines := strings.Split(strings.Trim(string(p), " \n\t"), "\n")
+	serverinfo := lines[1][1:]
+	playerinfo := lines[2 : len(lines)-1]
+
+	info := map[string]string{}
+	vars := strings.Split(serverinfo, "\\")
+
+	for i := 0; i < len(vars); i += 2 {
+		info[strings.ToLower(vars[i])] = vars[i+1]
+	}
+
+	playercount := len(playerinfo)
+	info["player_count"] = fmt.Sprintf("%d", playercount)
+
+	if playercount > 0 {
+		players := ""
+
+		for _, p := range playerinfo {
+			player := strings.SplitN(p, " ", 3)
+			players = fmt.Sprintf("%s,%s", players, player[2])
+		}
+
+		info["players"] = players[1:]
+	}
+
+	output := fmt.Sprintf(
+		"%s\n%s - %s/%s",
+		info["hostname"],
+		info["mapname"],
+		info["player_count"],
+		info["maxclients"])
+
+	pcount, _ := strconv.Atoi(info["player_count"])
+
+	if pcount > 0 {
+		players_array := strings.Split((info["players"])[1:len(info["players"])], "\",\"")
+		players := ""
+		for _, p := range players_array {
+			players = players + p + ", "
+		}
+		output = fmt.Sprintf("%s\n[`%s`]", output, players[:len(players)-3])
+	}
+
+	return output
 }
